2662811aa914907edd499b509690ed8bdbb0eabc
Glitch (lamp-poc) <none>
2020-10-16 22:37:51 +0000

    🌦👌 Checkpoint ./index.php:8045597/446

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<html>
-  <head>
-    <title>LAMP Proof of Concept</title>
-  </head>
-  <body>
-    <?php
-    $number = 5;
-    echo "The number of the day is $number!";
-    ?>
-  </body>
-</html>
+<?php
+require './vendor/autoload.php'
+echo $Parsedown->line('Hello _Parsedown_!');
+?>
