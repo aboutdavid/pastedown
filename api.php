a6abccfeeda14770d49e8f8777049cc253d89c59
Glitch (lamp-poc) <none>
2020-10-17 20:36:07 +0000

    🐙🍕 Checkpoint ./api.php:8045597/208

--- api.php
+++ api.php
@@ -4,7 +4,17 @@ $jsonobj = fread($file,filesize("database.json"));
 
 $arr = json_decode($jsonobj, true);
 
+$arr["v1"] = "test1"
+$arr["v2"] = "test2"
+$arr["v3"] = "test3"
+
 echo $arr["v1"];
 echo $arr["v2"];
 echo $arr["v3"];
+
+
+
+$save = json_encode($arr);
+fwrite($file,$save);
+fclose($file);
 ?>
