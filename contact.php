6a6b43a845fa6099dbf47e91e2ca040c27a8cb6e
Glitch (lamp-poc) <none>
2020-10-19 19:54:30 +0000

    📯🚩 Checkpoint ./contact.php:8045597/999 ./.htaccess:8045597/110

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen">
+</head>
+<body>
+  <form action="https://send.pageclip.co/1rqQVmbOXkLEmJ7RDAYgwMk3B7T1WI6J/Pastedown-Contact-Form" class="pageclip-form" method="post">
+  <!-- Replace these inputs with your own. Make sure they have a "name" attribute! -->
+
+  <!-- It looks like you are creating a contact form. These email and subject fields are special -->
+  <!-- See https://pageclip.co/docs#special-fields for more info -->
+  <input type="email" name="email" value="roscoe@example.com" />
+  <input type="text" name="subject" value="A contact subject" />
+
+  <!-- This button will have a loading spinner. Keep the inner span for best results. -->
+  <button type="submit" class="pageclip-form__submit">
+    <span>Send</span>
+  </button>
+</form>
+  <footer>
+    <script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8"></script>
+  </footer>
+</body>
