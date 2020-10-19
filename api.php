69ad6c128f7607b4e3c43271316f3bab220f8f16
Glitch (lamp-poc) <none>
2020-10-19 17:11:11 +0000

    🍗👩 Checkpoint ./api.php:8045597/2535 ./config.ini:8045597/145 ./.gitignore:8045597/11 ./config.ini.sample:8045597/257

--- api.php
+++ api.php
@@ -1,8 +1,25 @@
 <?php
+$ini = parse_ini_file('config.ini');
 die();
+
+define("RECAPTCHA_V3_SECRET_KEY", 'YOUR_SECRET_HERE');
+$token = $_POST['token'];
+$action = $_POST['action'];
+ 
+// call curl to POST request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+ 
+// verify the response
+if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
 $file = file_get_contents("database.json");
 $paste = $_REQUEST['paste'];
-$ini = parse_ini_file('config.ini');
 if ($paste === null or $paste === "") {
   echo "You can't have an empty paste.";
   die();
@@ -29,4 +46,8 @@ fwrite($fileobj,$encoded);
 fclose($fileobj);
 header("Location: /paste/" . $randomString);
 exit();
+  
+} else {
+echo "We think you might be a robot. Please try again later."
+}
 ?>
